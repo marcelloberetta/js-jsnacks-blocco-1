993b1768f6263062e01797d939e238ca9d5cdafa
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-11 19:27:35 +0200

    Javascript file with all solutions

---
+++ overall javascript.js
@@ -0,0 +1,117 @@
+// FILE CHE CONTIENE TUTTI I JAVASCRIPT DEGLI ESERCIZI JSNACK
+
+
+//  ************JSNACK 1************
+
+
+//AVVISIAMO L'UTENTE DEL FUNZIONAMENTO VISIVO DELLO SCRIPT
+alert ("Ciao! Facciamo un gioco: ti chiederò due numeri (diversi tra loro) e di questi ti indicherò il maggiore nella console del browser..cominciamo!");
+
+//VARIABILE CHE CONTIENE IL PRIMO NUMERO CHE L'UTENTE CI COMUNICHERA'
+var firstNumber = parseInt(prompt("Digita il primo numero"));
+
+//VARIABILE CHE CONTIENE IL SECONDO NUMERO CHE L'UTENTE CI COMUNICHERA'
+var secondNumber = parseInt(prompt("Ora digita il secondo numero"));
+
+//IMPOSTIAMO LA CONDIZIONE CON TRE POSSIBILI RISULTATI
+if(firstNumber > secondNumber){
+
+ console.log("Il numero più grande da te digitato è " + firstNumber)
+
+}
+
+else if(firstNumber < secondNumber){
+
+ console.log("Il numero più grande da te digitato è " + secondNumber)
+}
+
+else{
+ console.log("ERRORE -- I NUMERI DEVONO ESSERE DIVERSI")
+}
+
+
+//  ************JSNACK 2************
+
+
+alert("Ciao! Ora ti chiederò di digitare due parole (possibilmente di lunghezza diversa tra loro; ti mostrerò in console prima la più corta, poi la più lunga..iniziamo!");
+
+var firstWord = prompt ("Digita la prima parola");
+
+var secondWord = prompt ("Digita la seconda parola");
+
+if (firstWord.length > secondWord.length){
+ console.log(secondWord + " " + firstWord)
+}
+else if(firstWord.length < secondWord.length){
+ console.log(firstWord + " " + secondWord)
+}
+else{
+ console.log("ERRORE - LE PAROLE SONO LUNGHE UGUALI")
+}
+
+
+
+//  ************JSNACK 3************
+
+
+//CREO UNA VARIABILE CHE CONTERRA' LA SOMMA DEI NUMERI INSERITI DALL'UTENTE
+var sum = 0;
+
+//CICLO CHE CHE CHIEDE ALL'UTENTE DI INSERIRE UN NUMERO PER 10 VOLTE
+ for( var i=0;  i<10; i++){
+    var newNumber = parseInt(prompt("Inserisci un numero")) ;
+   sum +=  newNumber;
+   }
+
+   //STAMPO LA SOMMA
+   console.log(sum);
+
+
+
+//  ************JSNACK 4************
+
+
+
+//CREIAMO UN ARRAY CONTENENTE I NOMI DEGLI INVITATI
+var invited = ["Pippo", "pippo", "Pluto", "pluto", "Topolino", "topolino", "Minni", "minni", "Paperino", "paperino", "Paperina" , "paperina"];
+
+alert("Ciao! Benvenuto alla festa del Grande Gatsby, dimmi il tuo nome e verificherò se  è presente nella lista degli invitati");
+
+var user = prompt("Come ti chiami ?");
+
+userPass = false; //FLAG DI  VERIFICA
+
+for(i=0; i<invited.length; i++) {
+   var userTry = invited [i];  //VARIABILE TEMPORANEA
+   
+   if (userTry === user){
+      userPass = true;
+   }
+}
+
+if (userPass){
+   console.log("Sei nella lista degli invitati " +  user +  " , accomodati pure");
+}
+else{
+   console.log("Mi dispiace, " + user + " non sei stato invitato");
+}
+
+
+
+
+//  ************JSNACK 5************
+
+
+//CREIAMO UN ARRAY VUOTO
+var numbers = [ ];
+
+//CREIAMO UN CICLO PER CHIEDERE ALL'UTENTE DI INSERIRE 5 NUMERI
+ for(var i=0; i<5 ;  i++){
+   var userNumber = parseInt(prompt("Inserisci un numero"));
+   if(userNumber %2 !=0){
+      numbers.push(userNumber);
+   }
+}
+
+console.log(numbers);
+
