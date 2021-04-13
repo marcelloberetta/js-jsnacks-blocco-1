094bf8c2739d78b09fe7d5035a6168a0b473b04b
marcelloberetta <marcelloberetta4@gmail.com>
2021-04-13 19:03:13 +0200

    Update File

--- overall javascript.js
+++ overall javascript.js
@@ -134,3 +134,19 @@ console.log(somma);
 
 //  ************JSNACK 9************
 
+//INIZALIZZIAMO UNA VARIABILE CHE CONTERRA' IL RISULTATO DELLA SOMMA
+var somma= 0;
+
+//FACCIAMO UN CICLO CHE PER 10 VOLTE ESTRARRA' UN NUMERO
+for( var i=0; i<10; i++){
+   var numero = i+1
+   somma += numero;
+  
+   
+}
+
+console.log(somma); //SOMMA DEI PRIMI 10 NUMERI
+
+console.log(somma/10);  //MEDIA
+
+
